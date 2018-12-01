7499db327b733a7afe47b9eaed17c55fb85dbf8c
Tejas Bubane <tejasbubane@gmail.com>
2018-12-01 22:09:54 +0530

    Solve Day1 - part 2

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,4 +1,18 @@
-module Day1 (frequency) where
+module Day1 (frequency, duplicate) where
 
+import Data.Set
+
+-- part 1
 frequency :: [Integer] -> Integer
 frequency = sum
+
+-- part 2
+findDuplicate :: Integer -> Set Integer -> [Integer] -> Integer
+findDuplicate _ _ [] = undefined
+findDuplicate currentSum s (x:xs) =
+  if currentSum `member` s
+  then currentSum
+  else findDuplicate (currentSum + x) (insert currentSum s) xs
+
+duplicate :: [Integer] -> Integer
+duplicate = findDuplicate 0 empty . cycle
