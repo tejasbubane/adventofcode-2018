72e71d5ba9aacd06124f4cd44ba4cc9615401ed4
Tejas Bubane <tejasbubane@gmail.com>
2018-12-02 15:24:02 +0530

    Solve Day2 - part 2

--- src/Day2.hs
+++ src/Day2.hs
@@ -1,4 +1,4 @@
-module Day2 (checksum) where
+module Day2 (checksum, commonBoxId) where
 
 import Data.List (sort, group, find)
 
@@ -16,3 +16,28 @@ checksum xs =
   let cs = map (counts . group . sort) xs
       (count2, count3) = foldr (\(x, y) (acc2, acc3) -> (acc2 + x, acc3 + y)) (0, 0) cs
   in count2 * count3
+
+-- part 2
+
+match :: String -> String -> String
+match [] [] = ""
+match (x:xs) (y:ys) =
+  if x == y
+  then (x:(match xs ys))
+  else match xs ys
+match _ _ = error "input list lengths do not match"
+
+-- check if only one common char between two strings
+-- assuming two input lists are of equal length
+similarity :: String -> String -> (String, Int)
+similarity xs ys =
+  let common = xs `match` ys
+  in (common, length xs - length common)
+
+commonBoxId :: [String] -> String
+commonBoxId ids =
+  let shiftIds = tail ids ++ [head ids]
+      commonBox = find (\(_, diff) -> diff == 1) $ similarity <$> ids <*> shiftIds
+  in case commonBox of
+    Just (boxId, _) -> boxId
+    Nothing       -> error "Box not found!"
