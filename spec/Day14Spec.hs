d56cf19891e95210a414454504d196cf993f2477
Tejas Bubane <tejasbubane@gmail.com>
2018-12-24 23:37:54 +0530

    Solve Day 14 - part 2
    With hints from https://github.com/mstksg/advent-of-code-2018/blob/master/reflections.md#day-14

--- spec/Day14Spec.hs
+++ spec/Day14Spec.hs
@@ -13,3 +13,12 @@ specs = describe "Day 14" $ do
       part1 2018 `shouldBe` [5,9,4,1,4,2,9,8,8,2]
     it "works for puzzle input" $ do
       part1 540561 `shouldBe` [1,4,1,3,1,3,1,3,3,9]
+
+  describe "Part 2" $ do
+    it "works for given examples" $ do
+      part2 [5,1,5,8,9] `shouldBe` 9
+      part2 [0,1,2,4,5] `shouldBe` 5
+      part2 [9,2,5,1,0] `shouldBe` 18
+      part2 [5,9,4,1,4] `shouldBe` 2018
+    it "works for puzzle input" $ do
+      part2 [5,4,0,5,6,1] `shouldBe` 20254833
--- src/Day14.hs
+++ src/Day14.hs
@@ -1,6 +1,7 @@
 module Day14 where
 
 import qualified Data.Sequence as S
+import Data.List (isSuffixOf, inits, tails, isPrefixOf)
 
 type Pos = Int
 type Recipes = S.Seq Int
@@ -8,8 +9,8 @@ type Recipes = S.Seq Int
 digits :: Int -> [Int]
 digits = map (read . (:[])) . show
 
-makeRecipes :: [Int]
-makeRecipes = 3 : 7 : 1 : 0 : (go (S.fromList [3,7,1,0]) 0 1) where
+recipes :: [Int]
+recipes = 3 : 7 : 1 : 0 : (go (S.fromList [3,7,1,0]) 0 1) where
   go scores p1 p2 = newRecipes ++ go newScores newP1 newP2 where
     r1 = scores `S.index` p1
     r2 = scores `S.index` p2
@@ -20,4 +21,7 @@ makeRecipes = 3 : 7 : 1 : 0 : (go (S.fromList [3,7,1,0]) 0 1) where
     newP2 = (p2 + 1 + r2) `mod` l
 
 part1 :: Int -> [Int]
-part1 n = take 10 $ drop n makeRecipes
+part1 n = take 10 $ drop n recipes
+
+part2 :: [Int] -> Int
+part2 rs = length $ takeWhile (\r -> not (rs `isPrefixOf` r)) (tails recipes)
