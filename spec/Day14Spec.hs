5c62f3c6131d95a467691f0f8b2dfdc039f7417b
Tejas Bubane <tejasbubane@gmail.com>
2018-12-24 23:06:58 +0530

    Refactor Day 14 part 1 to generate infinite sequence of recipe scores

--- spec/Day14Spec.hs
+++ spec/Day14Spec.hs
@@ -5,12 +5,11 @@ import Day14
 
 specs :: SpecWith ()
 specs = describe "Day 14" $ do
-  describe "part 1" $ do
+  describe "Part 1" $ do
     it "works for given examples" $ do
-      part1 9 `shouldBe` "5158916779"
-      part1 5 `shouldBe` "0124515891"
-      part1 18 `shouldBe` "9251071085"
-      part1 2018 `shouldBe` "5941429882"
-
+      part1 9 `shouldBe` [5,1,5,8,9,1,6,7,7,9]
+      part1 5 `shouldBe` [0,1,2,4,5,1,5,8,9,1]
+      part1 18 `shouldBe` [9,2,5,1,0,7,1,0,8,5]
+      part1 2018 `shouldBe` [5,9,4,1,4,2,9,8,8,2]
     it "works for puzzle input" $ do
-      part1 540561 `shouldBe` "1413131339"
+      part1 540561 `shouldBe` [1,4,1,3,1,3,1,3,3,9]
--- src/Day14.hs
+++ src/Day14.hs
@@ -1,26 +1,23 @@
 module Day14 where
 
 import qualified Data.Sequence as S
-import Data.Maybe (fromMaybe)
-import Data.Foldable (toList)
 
 type Pos = Int
+type Recipes = S.Seq Int
 
 digits :: Int -> [Int]
 digits = map (read . (:[])) . show
 
-part1 :: Int -> String
-part1 reqRecipes = go (S.fromList [3,7,1,0]) 0 1 where
-  go :: S.Seq Int -> Pos -> Pos -> String
-  go scores p1 p2
-    | (S.length scores) >= (reqRecipes + 10) =
-      concat . map show . take 10 . drop reqRecipes . toList $ scores
-    | otherwise =
-        let r1 = fromMaybe (-1) $ S.lookup p1 scores
-            r2 = fromMaybe (-1) $ S.lookup p2 scores
-            newRecipes = S.fromList $ digits $ r1 + r2
-            newScores = (S.><) scores newRecipes
-            l = length newScores
-            newP1 = (p1 + 1 + r1) `mod` l
-            newP2 = (p2 + 1 + r2) `mod` l
-        in go newScores newP1 newP2
+makeRecipes :: [Int]
+makeRecipes = 3 : 7 : 1 : 0 : (go (S.fromList [3,7,1,0]) 0 1) where
+  go scores p1 p2 = newRecipes ++ go newScores newP1 newP2 where
+    r1 = scores `S.index` p1
+    r2 = scores `S.index` p2
+    newRecipes = digits $ r1 + r2
+    newScores = (S.><) scores (S.fromList newRecipes)
+    l = length newScores
+    newP1 = (p1 + 1 + r1) `mod` l
+    newP2 = (p2 + 1 + r2) `mod` l
+
+part1 :: Int -> [Int]
+part1 n = take 10 $ drop n makeRecipes
